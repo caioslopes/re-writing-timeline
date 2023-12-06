617c3a5440cf6b7ae5e274e90eaeee528d947ab6
gutsserrano <gutsserrano@gmail.com>
2023-12-06 14:29:34 -0300

    Corrigindo bug de não carregar imagem

--- js/model/Card.js
+++ js/model/Card.js
@@ -10,15 +10,42 @@ export default class Card{
     }
 
     async createCard(){
-        const api = new Api();
-        let n = this.#getRandomArbitrary(1960, 2023);
-        let i = this.#getRandomArbitrary(0, 19);
+        
+        let consult = false;
+        let temp = null;
 
-        const data = await api.consult(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&region=br&page=1&primary_release_year=${n}&sort_by=revenue.desc`);
-        let temp = data.results[i];
+        do{
+            temp = await this.tryConsult(temp);
+            if(temp.poster_path == null){
+                console.log("errei fui mlk");
+            }
+        }while(temp.poster_path == null);
+
+        /* if(temp.poster_path == null){
+           await this.createCard();
+           console.log("entrei e pedi dnv");
+        }else{
+            console.log(temp);
+
+            temp.release_date = temp.release_date.slice(0,4);
+            this.card = temp;
+        } */
+
+
+        console.log(temp);
 
         temp.release_date = temp.release_date.slice(0,4);
         this.card = temp;
+        
+    }
+
+    async tryConsult(temp){
+        const api = new Api();
+        let n = this.#getRandomArbitrary(1960, 2023);
+        let i = this.#getRandomArbitrary(0, 19);
+        const data = await api.consult(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&region=br&page=1&primary_release_year=${n}&sort_by=revenue.desc`);
+        temp = data.results[i];
+        return temp
     }
 
     #getRandomArbitrary(min, max) {
